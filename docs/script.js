9446f525b38d7343a24dd9444d05940ebe50420a
MoYusuff <gi6342@myamu.ac.in>
2023-05-23 01:17:50 +0530

    more changes

--- docs/script.js
+++ docs/script.js
@@ -1,38 +1,91 @@
 function sortList() {
-    var input = document.getElementById("integerList").value;
-    var integers = parseInput(input);
-    integers.sort(function(a, b) {
-      return a - b;
-    });
-    displayOutput(integers);
+  var input = document.getElementById("integerList").value;
+  var integers = parseInput(input);
+  integers.sort(function(a, b) {
+    return a - b;
+  });
+  displayOutput(integers);
+}
+
+function calculateMinimum() {
+  var input = document.getElementById("integerList").value;
+  var integers = parseInput(input);
+  var minimum = Math.min(...integers);
+  displayOutput(minimum);
+}
+
+function calculateMaximum() {
+  var input = document.getElementById("integerList").value;
+  var integers = parseInput(input);
+  var maximum = Math.max(...integers);
+  displayOutput(maximum);
+}
+
+function calculatePrimeNumbers() {
+  var input = document.getElementById("integerList").value;
+  var integers = parseInput(input);
+  var primeNumbers = findPrimeNumbers(integers);
+  displayOutput(primeNumbers);
+}
+
+function calculateMean() {
+  var input = document.getElementById("integerList").value;
+  var integers = parseInput(input);
+  var sum = integers.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+  var mean = sum / integers.length;
+  displayOutput(mean);
+}
+
+function calculateMedian() {
+  var input = document.getElementById("integerList").value;
+  var integers = parseInput(input);
+  integers.sort(function(a, b) {
+    return a - b;
+  });
+  var median;
+  if (integers.length % 2 === 0) {
+    var middle = integers.length / 2;
+    median = (integers[middle - 1] + integers[middle]) / 2;
+  } else {
+    var middleIndex = Math.floor(integers.length / 2);
+    median = integers[middleIndex];
   }
-  
-  function calculateMinimum() {
-    var input = document.getElementById("integerList").value;
-    var integers = parseInput(input);
-    var minimum = Math.min(...integers);
-    displayOutput(minimum);
+  displayOutput(median);
+}
+
+function parseInput(input) {
+  var inputArray = input.split(",").map(function(item) {
+    return parseInt(item.trim(), 10);
+  });
+  return inputArray;
+}
+
+function findPrimeNumbers(numbers) {
+  var primes = [];
+  for (var i = 0; i < numbers.length; i++) {
+    var num = numbers[i];
+    if (isPrime(num)) {
+      primes.push(num);
+    }
   }
-  
-  function calculateAverage() {
-    var input = document.getElementById("integerList").value;
-    var integers = parseInput(input);
-    var sum = integers.reduce(function(a, b) {
-      return a + b;
-    }, 0);
-    var average = sum / integers.length;
-    displayOutput(average);
+  return primes;
+}
+
+function isPrime(number) {
+  if (number < 2) {
+    return false;
   }
-  
-  function parseInput(input) {
-    var inputArray = input.split(",").map(function(item) {
-      return parseInt(item.trim(), 10);
-    });
-    return inputArray;
+  for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
   }
-  
-  function displayOutput(output) {
-    var outputElement = document.getElementById("output");
-    outputElement.innerHTML = "Output: " + output;
-  }
-  
+  return true;
+}
+
+function displayOutput(output) {
+  var outputElement = document.getElementById("output");
+  outputElement.innerHTML = "Output: " + output;
+}
